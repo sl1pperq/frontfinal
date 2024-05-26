5ce6a2727138a9fedfbc33ccf9b6cbcab3e7da96
Sergey Averin <122549207+sl1pperq@users.noreply.github.com>
2024-05-26 20:22:40 +0300

    HELLO_2

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://back.banjosurf.ru/api' // TODO: https://back.banjosurf.ru/api
+export const BASE_URL = 'http://127.0.0.1:3002/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
